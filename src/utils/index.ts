5d6144cf81df416eb48f0352b2b7bddd195e63d1
15506 <15506531157@163.com>
2024-05-10 15:28:30 +0800

    eslint

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,18 @@
 import { md5 } from "js-md5";
 
+/**
+ * 密码加密
+ * @param password 原始密码
+ * @returns 加密后的密码
+ */
 export const passwordEncryption = (password: string): string => {
 	return md5(password + "392a2f71-4bd5-44fc-a6fa-b3448085a4c9");
 };
 
+/**
+ * 基础的上传图片地址
+ * @returns 地址
+ */
 export const baseUploadImgUrl = (): string => {
 	return "/api/upload";
 };
